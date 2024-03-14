16a51a66548d9fad8ed4f086e2087c546d4e01b5
Nicolas Ruiz <nicolasruiz81@gmail.com>
2024-03-14 16:24:36 -0400

    ✨ Revamp my prev nvim config

---
+++ lua/custom/plugins/harpoon.lua
@@ -0,0 +1,34 @@
+return {
+  'ThePrimeagen/harpoon',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+  },
+  keys = {
+    '<a-h>',
+    '<a-j>',
+    '<a-k>',
+    '<a-l>',
+    '<leader>a',
+    '<C-e>',
+  },
+  config = function()
+    local mark = require 'harpoon.mark'
+    local ui = require 'harpoon.ui'
+
+    vim.keymap.set('n', '<leader>a', mark.add_file)
+    vim.keymap.set('n', '<C-e>', ui.toggle_quick_menu)
+
+    vim.keymap.set('n', '<a-h>', function()
+      ui.nav_file(1)
+    end)
+    vim.keymap.set('n', '<a-j>', function()
+      ui.nav_file(2)
+    end)
+    vim.keymap.set('n', '<a-k>', function()
+      ui.nav_file(3)
+    end)
+    vim.keymap.set('n', '<a-l>', function()
+      ui.nav_file(4)
+    end)
+  end,
+}
---
+++ lua/custom/plugins/toggle-term.lua
@@ -0,0 +1,36 @@
+return {
+  'akinsho/toggleterm.nvim',
+  keys = {
+    { [[<C-\>]] },
+    { '<leader>0', '<Cmd>2ToggleTerm<Cr>', desc = 'Terminal #2' },
+  },
+  cmd = { 'ToggleTerm', 'TermExec' },
+  opts = {
+    size = 30,
+    hide_numbers = true,
+    open_mapping = [[<C-\>]],
+    shade_filetypes = {},
+    shade_terminals = true,
+    shading_factor = 0.3,
+    start_in_insert = true,
+    persist_size = true,
+    direction = 'float',
+    winbar = {
+      enabled = false,
+      name_formatter = function(term)
+        return term.name
+      end,
+    },
+  },
+  config = function(_, options)
+    require('toggleterm').setup(options)
+
+    vim.keymap.set('t', '<esc>', [[<C-\><C-n>]])
+    vim.keymap.set('t', 'jk', [[<C-\><C-n>]])
+    vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]])
+    vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]])
+    vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]])
+    vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]])
+    vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]])
+  end,
+}

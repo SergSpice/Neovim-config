e050e516f4ade5bb288334d10ed7d1e3df876dd4
Nicolas Ruiz <nicolasruiz81@gmail.com>
2024-04-22 09:13:49 -0400

    ✨ Added gx and fixed copilot

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -51,7 +51,7 @@ return {
       keymaps = {
         ['g?'] = 'actions.show_help',
         ['<CR>'] = 'actions.select',
-        ['<C-s>'] = 'actions.select_vsplit',
+        ['<C-v>'] = 'actions.select_vsplit',
         ['<C-h>'] = 'actions.select_split',
         ['<C-t>'] = 'actions.select_tab',
         ['<C-p>'] = 'actions.preview',
@@ -70,7 +70,7 @@ return {
       use_default_keymaps = false,
       view_options = {
         -- Show files and directories that start with "."
-        show_hidden = false,
+        show_hidden = true,
         -- This function defines what is considered a "hidden" file
         is_hidden_file = function(name, bufnr)
           return vim.startswith(name, '.')
--- lua/custom/plugins/package-info.lua
+++ lua/custom/plugins/package-info.lua
@@ -6,6 +6,7 @@ return {
   config = function()
     local package_info = require 'package-info'
     vim.keymap.set('n', '<leader>nt', package_info.toggle, { silent = true, noremap = true })
+    vim.keymap.set('n', '<leader>ntu', package_info.update, { silent = true, noremap = true })
     package_info.setup {
       hide_up_to_date = true,
       hide_unstable_versions = true,

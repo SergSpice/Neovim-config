6c949445585afdb928d86bda72f89d858d630a7d
Nicolas Ruiz <nicolasruiz81@gmail.com>
2024-06-19 16:11:02 -0400

    ✨ Added some configs to gruvbox material after neovim upgrade

--- init.lua
+++ init.lua
@@ -389,6 +389,8 @@ require('lazy').setup({
 
         let g:gruvbox_material_background = 'hard'
         let g:gruvbox_material_better_performance = 1
+        let g:gruvbox_material_foreground = 'mix'
+        let g:gruvbox_material_transparent_background = 1
 
         colorscheme gruvbox-material
       ]]
